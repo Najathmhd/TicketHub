f9000c686c3be23b1e443972071116317a3c8570
Najathmhd <najamhd037@gmail.com>
2026-02-16 12:26:37 +0530

    Review Scalffold created

---
+++ TicketHub/Controllers/ReviewsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TicketHub.Models;
+
+namespace TicketHub.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ReviewsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reviews
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Reviews.Include(r => r.Event).Include(r => r.Member);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Reviews/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews
+                .Include(r => r.Event)
+                .Include(r => r.Member)
+                .FirstOrDefaultAsync(m => m.ReviewId == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // GET: Reviews/Create
+        public IActionResult Create()
+        {
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId");
+            ViewData["MemberId"] = new SelectList(_context.Members, "MemberId", "MemberId");
+            return View();
+        }
+
+        // POST: Reviews/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ReviewId,Rating,Comment,ReviewDate,MemberId,EventId")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(review);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId", review.EventId);
+            ViewData["MemberId"] = new SelectList(_context.Members, "MemberId", "MemberId", review.MemberId);
+            return View(review);
+        }
+
+        // GET: Reviews/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews.FindAsync(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId", review.EventId);
+            ViewData["MemberId"] = new SelectList(_context.Members, "MemberId", "MemberId", review.MemberId);
+            return View(review);
+        }
+
+        // POST: Reviews/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ReviewId,Rating,Comment,ReviewDate,MemberId,EventId")] Review review)
+        {
+            if (id != review.ReviewId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(review);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReviewExists(review.ReviewId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId", review.EventId);
+            ViewData["MemberId"] = new SelectList(_context.Members, "MemberId", "MemberId", review.MemberId);
+            return View(review);
+        }
+
+        // GET: Reviews/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews
+                .Include(r => r.Event)
+                .Include(r => r.Member)
+                .FirstOrDefaultAsync(m => m.ReviewId == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var review = await _context.Reviews.FindAsync(id);
+            if (review != null)
+            {
+                _context.Reviews.Remove(review);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReviewExists(int id)
+        {
+            return _context.Reviews.Any(e => e.ReviewId == id);
+        }
+    }
+}
